23ea5ec094db7d95e9dfc431839e48e48a004d69
alxl <alx.l@gmx.com>
2014-05-07 22:02:35 +0000

    General bugfixing and cleanup

--- app/models/member/user.rb
+++ app/models/member/user.rb
@@ -19,7 +19,7 @@ module Member
     validates :last_name, presence: true, length: { maximum: 20 }, format: { with: /\A[a-zA-Z]+\Z/ }
     validates :username, presence: true, length: { maximum: 20 }, uniqueness: {case_sensitive: false}, format: { with: /\A[a-zA-Z0-9]+\Z/ }
     validates :role, presence: true, inclusion: { :in => SimpleRoles.config.valid_roles }
-    validates :description, length: { maximum: 255 }, format: { with: /\A[a-zA-Z]+\Z/ }, allow_blank: true, on: :update
+    validates :description, length: { maximum: 512 }, allow_blank: true, on: :update
 
     # Override devise method to allow username and email sign in
     def self.find_first_by_auth_conditions(warden_conditions)
