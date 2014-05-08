6a9728c0f10709d33b80ccfac651684d2685d799
alxl <alx.l@gmx.com>
2014-05-08 13:45:06 +0000

    General bugfixing and cleanup

--- app/controllers/member/application_controller.rb
+++ app/controllers/member/application_controller.rb
@@ -2,5 +2,7 @@ module Member
   class Member::ApplicationController < ::ApplicationController
     before_filter :authenticate_user!
     
+    # Include helpers from common domain engine
+    helper CommonDomain::Engine.helpers
   end
 end
